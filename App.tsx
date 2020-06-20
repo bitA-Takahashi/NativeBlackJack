013de50f0bbee0f4a63a73e65c44227f96e381d7
YutoTakahashi <yuto.takahashi2@persol.co.jp>
2020-06-21 02:55:30 +0900

    feat: ベース環境構築（lint prettier styled-component）

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { DefaultLayout } from './src/components/templates/DefaultLayout';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <DefaultLayout />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
